df30e6853d923b9a4a713722ef96ec7516e86d08
AlexisInsMu <109385696+AlexisInsMu@users.noreply.github.com>
2023-09-17 00:39:58 -0600

    Delete out directory

--- out/build/x64-Debug/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: F:/c_projects/the_c_book
-# Build directory: F:/c_projects/the_c_book/out/build/x64-Debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
